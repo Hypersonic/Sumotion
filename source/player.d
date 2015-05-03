c4215e4225da4dcd8164493b5a40668a873fcf96
Josh Hofing <joshhofing@gmail.com>
2015-05-02 20:17:37 -0400

    Player class cleanup

--- source/player.d
+++ source/player.d
@@ -4,7 +4,7 @@ class Player {
           ctrl_vx = 0, ctrl_vy = 0, // Player controlled velocity
           max_speed = .01, // Max control velocity
           friction = .9, // Friction to apply
-          mass = .2; // Effective mass, changed by player input
+          mass = .2, // Effective mass, changed by player input
+          recent_presses = 0, mass_coefficient = 0;
     bool up, down, left, right, crash_circle; // Buttons pressed for directions
-    float recent_presses = 0, mass_coefficient = 0;
 }
